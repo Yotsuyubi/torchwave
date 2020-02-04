4758e3b2af44a47c6aa57dee7af34c994fa7518e
Yotsuyubi <yotsuyubi.rosamu@gmail.com>
2020-02-04 17:08:22 +0900

    wavelet

---
+++ src/torchwave/transforms/wavelet.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch
+import scipy.signal as sig
+import scipy
+from typing import Union, NewType, Optional, List, Any, Callable
+
+
+
+class Wavelet(object):
+    """Mother Wavelet for pytorch transform
+    Args:
+        sampling_rate (int, optional): sampling rate of input signal.
+                                       `None` to be same as inputed data.
+                                       Default: `None`
+        freq_reso (int, optional): resolution of frequency domain. `None` to be 100.
+                                   Default: `None`
+        w (int, optional): omega of scipy.signal.cwt. `None` to be `6`.
+                           Default: `None`
+        abs (bool, optional): make output absolute value or not.
+                              Default: `True`
+    Returns:
+        numpy.ndarray[np.float or np.complex] with shape (freq_reso, len(input signal)).
+    """
+    def __init__(self,
+                *,
+                sampling_rate: Optional[int]=None,
+                freq_reso: Optional[int]=None,
+                w: Optional[int]=None,
+                abs: bool=True):
+        self.sampling_rate: Optional[int] = sampling_rate
+        self.freq_reso: int = freq_reso if freq_reso is not None else 100
+        self.w: int = w if w is not None else 6
+        self.is_abs: bool = abs
+
+    def __call__(self, data):
+        """
+        site: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.morlet2.html#scipy.signal.morlet2
+        """
+        fs: int = self.sampling_rate if self.sampling_rate is not None else len(data)
+        freq: np.ndarray[np.float64] = np.linspace(1, fs/2, self.freq_reso)
+        widths: np.ndarray[np.float64] = self.w*fs / (2*freq*scipy.pi)
+        cwtmatr = sig.cwt(data, sig.morlet2, widths, w=self.w)
+        cwtmatr = np.abs(cwtmatr) if self.is_abs is True else cwtmatr
+        return cwtmatr
---
+++ tests/transforms/test_wavelet.py
@@ -0,0 +1,18 @@
+from torchwave.transforms.wavelet import Wavelet
+import numpy as np
+import pytest
+
+def test_wavelet():
+    noise = np.random.randn(512)
+    assert Wavelet().sampling_rate == None
+    assert Wavelet().freq_reso == 100
+    assert Wavelet()(noise).shape == (100,512)
+    assert Wavelet(sampling_rate=128)(noise).shape == (100,512)
+    assert Wavelet(freq_reso=80)(noise).shape == (80,512)
+    assert Wavelet()(noise).dtype == 'float64'
+    assert Wavelet(abs=False)(noise).dtype == 'complex128'
+
+    with pytest.raises(TypeError):
+        Wavelet(freq_reso=0.1, sampling_rate=512)(noise)
+        Wavelet(freq_reso=80, sampling_rate=0.512)(noise)
+        Wavelet(freq_reso=80, sampling_rate=512, w=0.1)(noise)
