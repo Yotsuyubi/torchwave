c3a4d7b78d947e4599be29f26996c1be351caf2f
Yotsuyubi <yotsuyubi.rosamu@gmail.com>
2020-02-06 16:34:17 +0900

    suppress UserWarning on acc

--- src/torchwave/metrics/acc.py
+++ src/torchwave/metrics/acc.py
@@ -1,9 +1,12 @@
 import numpy as np
 import torch
+import warnings
 
 def acc(y_hat, y):
-    y_hat = torch.tensor(y_hat).cpu().numpy()
-    y = torch.tensor(y).cpu().numpy()
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', UserWarning)
+        y_hat = torch.tensor(y_hat).cpu().numpy()
+        y = torch.tensor(y).cpu().numpy()
 
     if len(y.shape) == 2:   # if y is one-hot vector
         y = np.argmax(y, axis=1) # to index
