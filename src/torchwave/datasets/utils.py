f969571088ef23a639471187b9287b0a6f08c8a5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 23:17:32 +0000

    Adding tarfile member sanitization to extractall()

--- src/torchwave/datasets/utils.py
+++ src/torchwave/datasets/utils.py
@@ -84,7 +84,26 @@ def extract(filename):
     dirname = os.path.dirname(filename)
     basename = basename_without_ext(filename)
     with tarfile.open(filename, 'r:*') as tar:
-        tar.extractall(dirname)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, dirname)
         os.remove(filename)
     return dirname+'/'+basename
 
